3cc5ce0be7e768bb2a2a9204bd3ca3d8ee58fc55
Hernando Monge <hemogem@gmail.com>
2024-10-11 21:45:10 -0600

    finalizacion del store

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -12,6 +12,8 @@ const state = {
         new Todo('Piedra del infinito'),
         new Todo('Piedra del tiempo'),
         new Todo('Piedra de marfil'),
+        new Todo('Piedra del poder'),
+        new Todo('Piedra del campo'),
     ],
     filter: Filters.All,
 }
@@ -21,6 +23,77 @@ const initStore = () => {
     console.log('InitStore');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+const getTodos = (filter = Filters.All) => {
+    switch (filter) {
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed:
+            return state.todos.filter(todo => todo.done);
+        case Filters.Pending:
+            return state.todos.filter(todo => !todo.done);
+        default:
+            throw new Error(`Option ${filter} is not valid.`)
+    }
+}
+
+/**
+ * 
+ * @param {string} description 
+ */
+const addTodo = (description) => {
+    if (!description) throw new Error('Description is required.');
+    state.todos.push(new Todo(description));
+}
+
+/**
+ * 
+ * @param {string} todoId 
+ */
+const toggleTodo = (todoId) => {
+    state.todos = state.todos.map(todo => {
+        if (todo.id === todoId) {
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
+}
+
+/**
+ * 
+ * @param {string} todoId
+ */
+const deleteTodo = (todoId) => {
+    state.todos = state.todos.filter(todo => todo.id !== todoId);
+}
+
+const deleteCompleted = () => {
+    state.todos = state.todos.filter(todo => todo.done);
+}
+
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = (newFilter = Filters.All) => {
+    state.filter = newFilter;
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
 export default {
     initStore,
+    loadStore,
+    addTodo,
+    toggleTodo,
+    deleteTodo,
+    deleteCompleted,
+    setFilter,
+    getCurrentFilter,
+    getTodos,
 }
